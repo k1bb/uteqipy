bfe5e3b38bb2910015cc3264d58f94dde58fd8c3
k1bb <k1bridgebook@g.ecc.u-tokyo.ac.jp>
2025-05-13 09:27:30 +0900

    Wrote dependency on netcdf4 and dask.

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
         name="uteqipy", 
-        version="0.0.1", 
+        version="0.0.3", 
         author="Keiichi Hashimoto", 
         author_email="khashimoto@eps.s.u-tokyo.ac.jp", 
         packages=find_packages(), 
         install_requires=[
             "matplotlib", 
             "numpy", 
+            "dask", 
+            "Netcdf4", 
             "pandas", 
             "xarray", 
             "pillow", 
